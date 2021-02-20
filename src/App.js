f96391440ac577b22a7693c657ceb7099666653e
Kurt Choi <choi.kurt3@gmail.com>
2021-02-20 16:23:30 -0500

    add page routing via BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Box } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import './App.scss';
+import Main from './components/Main';
+import Profile from './components/Profile';
 
 function App() {
-  
+  const location = useLocation();
 
   return (
     <div className="App">
-      <Box>
-        
-      </Box>
+      <Switch location={location} key={location.pathname}>
+        <Route exact path='/' component={Main} />
+        <Route path='/profile' component={Profile} />
+      </Switch>
+      
     </div>
   );
 }
