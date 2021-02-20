da716fb9f88f4de8a939b9dc464b1dc2e32fef7b
Julie Lee <julielee9067@gmail.com>
2021-02-19 22:12:47 -0500

    fea) Edit App.js to make a connection between flask and react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
-      Hi
+      <header className="App-header">
+
+        ... no changes in this part ...
+
+        <p>The current time is {currentTime}.</p>
+      </header>
     </div>
   );
 }
