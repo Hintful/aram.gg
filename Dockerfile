54ca8a40e05f04d629d6abcb5b5707239a4dab4f
Julie Lee <julielee9067@gmail.com>
2021-02-23 14:07:02 -0500

    Add mariadb instead

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ EXPOSE 3000
 EXPOSE 35729
 
 # Set working directory
-WORKDIR /app
+WORKDIR /api
+
+RUN apt-get update -y
+RUN apt-get install -y python3-pip python-dev build-essential
 
 # Add /app/node_modules/.bin to environment variables
 ENV PATH /app/node_modules/.bin:$PATH
@@ -17,7 +20,7 @@ COPY package-lock.json /app/package-lock.json
 RUN npm install
 
 # Add React app to working directory
-ADD . /app
+ADD . /api
 
 # Start the React app
 CMD ["npm", "start"]
--- api/app.py
+++ api/app.py
@@ -1,37 +1,19 @@
-import datetime
-import os
-
-from flask import Flask, Response
-from flask_mongoengine import MongoEngine
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {
-    "host": os.environ["MONGODB_HOST"],
-    "username": os.environ["MONGODB_USERNAME"],
-    "password": os.environ["MONGODB_PASSWORD"],
-    "db": "webapp",
-}
-
-db = MongoEngine()
-db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:password@localhost/aramgg"
+db = SQLAlchemy(app)
 
 
-class Todo(db.Document):
-    title = db.StringField(max_length=60)
-    text = db.StringField()
-    done = db.BooleanField(default=False)
-    pub_date = db.DateTimeField(default=datetime.datetime.now)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
-@app.route("/api")
-def index():
-    Todo.objects().delete()
-    Todo(title="Simple todo A", text="12345678910").save()
-    Todo(title="Simple todo B", text="12345678910").save()
-    Todo.objects(title__contains="B").update(set__text="Hello world")
-    todos = Todo.objects().to_json()
-    return Response(todos, mimetype="application/json", status=200)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    def __repr__(self):
+        return f"<User {self.username}"
