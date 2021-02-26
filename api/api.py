27a763659c1e3db8cbc08e402726520a5d6e74b6
Julie Lee <julielee9067@gmail.com>
2021-02-26 00:34:45 -0500

    Refactor api.py to RiotApiRequests class instead of functions

--- api/api.py
+++ api/api.py
@@ -1,47 +1,67 @@
+from typing import List, Dict
+
 import requests
 
 API_KEY = "RGAPI-66c4b81c-e4e7-409c-bd29-b4c90526d3ec"
-params = {"api_key": API_KEY}
-
-def get_account_id(summoner_name):
-# def getAccountID(summmoner_name):
-    url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}"
-    
-    r = requests.get(url=url, params=params)
-    data = r.json()
-
-    return {"accountId": data["accountId"], "profileIconId": data["profileIconId"], "summonerLevel": data["summonerLevel"]}
-
-def get_matchlist(accountId):
-    url = f"https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{accountId}"
-    r = requests.get(url=url, params=params)
-    data = r.json()
-
-    return data
-
-def get_match(matchId):
-    url = f"https://na1.api.riotgames.com/lol/match/v4/matches/{matchId}"
-    r = requests.get(url=url, params=params)
-    data = r.json()
-
-    return data
+BASE_URL = "https://na1.api.riotgames.com"
+PARAMS = {"api_key": API_KEY}
 
 
-if __name__ == '__main__':
-    account_info = (get_account_id('JuIs'))
-    matchlist = list(map(lambda x: x["gameId"], get_matchlist(account_info["accountId"])["matches"]))
-    # print(matchlist[0])
+class RiotApiRequests:
+    def __init__(self, summoner_name: str, request_limit: int):
+        self.summoner_name = summoner_name
+        self.request_limit = request_limit
 
-    match_datas = []
-    for match in matchlist:
-        match_data = get_match(match)
-        if(match_data["gameMode"] == "ARAM"):
-            print(match_data)
-            break
-            match_datas.append(match_data)
-    
-    # print(match_datas[0])
-    
-    
+    def get_account_info(self) -> Dict:
+        """ Get account information based on the summoner name """
+
+        url = f"{BASE_URL}/lol/summoner/v4/summoners/by-name/{self.summoner_name}"
+        request = requests.get(url=url, params=PARAMS)
+        data = request.json()
+
+        return {
+            "account_id": data["accountId"],
+            "profile_icon_id": data["profileIconId"],
+            "summoner_level": data["summonerLevel"]
+        }
+
+    @staticmethod
+    def get_match_list(account_id: str) -> List[int]:
+        """ Get list of game IDs based on the summoner's account ID """
+
+        url = f"{BASE_URL}/lol/match/v4/matchlists/by-account/{account_id}"
+        request = requests.get(url=url, params=PARAMS)
+        match_list = request.json()
+
+        return [match["gameId"] for match in match_list["matches"]]
+
+    @staticmethod
+    def get_match_data(match_id: int) -> Dict:
+        """ Get information about specific match based on the match ID """
+
+        url = f"{BASE_URL}/lol/match/v4/matches/{match_id}"
+        request = requests.get(url=url, params=PARAMS)
+        data = request.json()
+
+        return data
+
+    def get_total_match_info(self) -> Dict:
+        """ Return combined dictionary with relevant match information """
+
+        aram_list = list()
+        info_dict = self.get_account_info()
+        match_list = self.get_match_list(account_id=info_dict["account_id"])
+
+        for index, match in zip(range(self.request_limit), match_list):
+            match_data = self.get_match_data(match)
+            if match_data["gameMode"] == "ARAM":
+                aram_list.append(match_data)
+
+        info_dict["match_list"] = aram_list
+        return info_dict
 
 
+if __name__ == "__main__":
+    riot_api = RiotApiRequests(summoner_name="juis", request_limit=10)
+    info = riot_api.get_total_match_info()
+    print(info)
