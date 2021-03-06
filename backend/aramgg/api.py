6c7ca222b45107ec707c4f3a14af85d2e54c7bce
Julie Lee <julielee9067@gmail.com>
2021-03-06 17:42:31 -0500

    Add env variable for api key

--- backend/aramgg/api.py
+++ backend/aramgg/api.py
@@ -9,9 +9,8 @@ django.setup()
 import requests
 from aramgg.models import Champion, User
 
-API_KEY = "RGAPI-5e8ec849-761c-4e43-a397-b004ba28a23d"
 BASE_URL = "https://na1.api.riotgames.com"
-PARAMS = {"api_key": API_KEY}
+PARAMS = {"api_key": os.environ["RIOT_API_KEY"]}
 
 
 class RiotApiRequests:
