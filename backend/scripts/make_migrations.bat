8bf19986711362db5e50057696868c94b7d8c5a7
Julie Lee <julielee9067@gmail.com>
2021-03-07 16:20:55 -0500

    Update migration command

--- backend/scripts/make_migrations.bat
+++ backend/scripts/make_migrations.bat
@@ -1,1 +1,3 @@
 docker exec -it aramgg_api_1 python manage.py makemigrations
+docker exec -it aramgg_api_1 python manage.py migrate --fake aramgg zero
+docker exec -it aramgg_api_1 python manage.py migrate
