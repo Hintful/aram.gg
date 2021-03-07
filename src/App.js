fc6c7dcef524afcfcf4bff12c2774e0f45a52e7f
Hintful <choi.kurt3@gmail.com>
2021-03-06 20:02:49 -0500

    Add redirection to main-page for non-existant route paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import './App.scss';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
@@ -14,8 +14,10 @@ function App() {
       <Navbar />
       <Switch location={location} key={location.pathname}>
         <Route exact path='/' component={Main} />
-        {/* <Route path='/profile' component={Profile} /> */}
         <Route path='/profile/:id' component={Profile} />
+        <Route path='*'>
+          <Redirect to='/'/>
+        </Route>
       </Switch>
       
     </div>
--- src/components/IconBox.js
+++ src/components/IconBox.js
@@ -1,6 +1,6 @@
 import { Image } from '@chakra-ui/image';
 import { Box } from '@chakra-ui/layout';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const IconBox = ({ profile_icon_id, level }) => {
   return (
