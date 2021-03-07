d6734eebdb99ba309762ba183d8adfd86791f7ee
Hintful <choi.kurt3@gmail.com>
2021-03-06 23:50:56 -0500

    Champion stats added - design change needed

--- src/components/ChampionStats.js
+++ src/components/ChampionStats.js
@@ -1,5 +1,7 @@
+import { StarIcon } from '@chakra-ui/icons';
 import { Image } from '@chakra-ui/image';
-import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
+import { Box, Divider, Flex, HStack, Spacer, Text } from '@chakra-ui/layout';
+import { Stat, StatLabel, StatNumber, StatHelpText, StatArrow, StatGroup } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import champion_data_json from './json/champion.json';
 
@@ -31,6 +33,49 @@ const getURLName = (name) => {
   }
 }
 
+const getKDAColor = (kda) => {
+  if (kda < 1.5) { return '#888888'; }
+  else if (kda < 2.3) { return 'black'; }
+  else if (kda < 3.1) { return '#90ee90'; }
+  else if (kda < 4) { return '#87cefa'; }
+  else if (kda < 5) { return '#ffa500'; }
+  else { return '#ff4500'; }
+}
+
+const roundNumber = (num) => {
+  return (Math.round(num * 10) / 10).toFixed(1);
+}
+
+const getKDAElement = (stats) => {
+  const kda = roundNumber((stats.kill + stats.assist) / stats.death);
+
+  return (
+    <span style={{ color: getKDAColor(kda) }}>
+      {kda}
+    </span>
+  )
+}
+
+const formatNumber = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const kdaStarRating = (stats) => {
+  const kda = roundNumber((stats.kill + stats.assist) / stats.death);
+  let star = 0;
+  if (kda < 1.5) { star = 1; }
+  else if (kda < 2.3) { star = 2; }
+  else if (kda < 3.1) { star = 3; }
+  else if (kda < 4) { star = 4; }
+  else { star = 5; }
+
+  return (
+     Array(5).fill("").map((_, i) => (
+      <StarIcon w={3} h={3} mt="7px" key={i} color={i < star ? "yellow.400" : "gray.500"} />
+    ))
+  )
+}
+
 const ChampionStats = ({ stats }) => {
   const [championData, setChampionData] = useState([]);
   const [championName, setChampionName] = useState('');
@@ -50,7 +95,7 @@ const ChampionStats = ({ stats }) => {
     <Flex direction="row" className="champion-stats">
       <Flex direction="column" justify="center" align="center" className="champion-icon">
         { championName !== '' ?
-          <Image mb={2} className="champion-icon-image" src={`http://ddragon.leagueoflegends.com/cdn/11.5.1/img/champion/${getURLName(championName)}.png`} />
+          <Image mb={1} className="champion-icon-image" src={`http://ddragon.leagueoflegends.com/cdn/11.5.1/img/champion/${getURLName(championName)}.png`} />
           :
           <div style={{ background: 'black' }}>
           </div>
@@ -58,6 +103,39 @@ const ChampionStats = ({ stats }) => {
         <Flex className="champion-name-label">
           <Text fontSize="sm">{championName}</Text>
         </Flex>
+      </Flex>
+      <Flex>
+        <HStack ml={3} spacing="40px">
+          <Stat>
+            <StatLabel>Wins</StatLabel>
+            <StatNumber color="blue.300">{stats.win}</StatNumber>
+            <StatHelpText>Games Won</StatHelpText>
+          </Stat>
+          <Stat>
+            <StatLabel>Losses</StatLabel>
+            <StatNumber color="red.300">{stats.loss}</StatNumber>
+            <StatHelpText>Games Lost</StatHelpText>
+          </Stat>
+          {/* <Divider orientation="vertical" /> */}
+          <Stat width="100px">
+            <StatLabel>KDA</StatLabel>
+            {/* <StatNumber>{`${roundNumber((stats.kill + stats.assist) / stats.death)}`}</StatNumber> */}
+            <StatNumber>{ getKDAElement(stats) }</StatNumber>
+            <StatHelpText>
+              { kdaStarRating(stats) }
+            </StatHelpText>
+          </Stat>
+          <Stat width="120px">
+            <StatLabel>Damage Done</StatLabel>
+            <StatNumber>{formatNumber(Math.round(stats.total_damage_done / (stats.win + stats.loss)))}</StatNumber>
+            <StatHelpText>Per Game</StatHelpText>
+          </Stat>
+          <Stat width="120px">
+            <StatLabel>Damage Taken</StatLabel>
+            <StatNumber>{formatNumber(Math.round(stats.total_damage_taken / (stats.win + stats.loss)))}</StatNumber>
+            <StatHelpText>Per Game</StatHelpText>
+          </Stat>
+        </HStack>
       </Flex>
 
     </Flex>
