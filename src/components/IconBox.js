2831dc8c41b8db2874d158ab072bd5051a5b362e
Hintful <choi.kurt3@gmail.com>
2021-03-09 22:28:47 -0500

    Add stylized border around profile icon based on star rating

--- src/components/IconBox.js
+++ src/components/IconBox.js
@@ -14,11 +14,23 @@ const getStarRatingStyle = (star) => {
   else { return { color: '#ff4500', textShadow: '0px 0px 4px #ff4500' }; }
 }
 
+const getBorderStyle = (star) => {
+  if (star < 2 || !star) { return {
+    border: "1px solid black"
+  }} else if (star < 3) { return {
+    border: "3px solid #90ee90"
+  }} else if (star < 4) { return {
+    border: "5px inset #87cefa"
+  }} else if (star < 5) { return {
+    border: "7px groove #ffa500"
+  }}
+}
+
 const IconBox = ({ profile_icon_id, level, totalKDA = null, performance = null, showStarRating = true }) => {
   return (
     <VStack mb={5}>
       { profile_icon_id ?
-        <Box className="icon-box" boxSize="100px" mb={-2}>
+        <Box className="icon-box" boxSize="100px" mb={-2} style={getBorderStyle(((performance * 5) + getKDAStarRating(totalKDA)) / 2)}>
           <Image className="icon-box-image" src={`http://ddragon.leagueoflegends.com/cdn/11.5.1/img/profileicon/${profile_icon_id}.png`} />
           <Box className="icon-box-level-badge">
             {level}
