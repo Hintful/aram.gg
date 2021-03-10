e00960c0928a8b3aeefe0964fdb081da028cce82
Hintful <choi.kurt3@gmail.com>
2021-03-10 00:26:04 -0500

    Add Tooltip information to overall star rating

--- src/components/IconBox.js
+++ src/components/IconBox.js
@@ -1,29 +1,42 @@
 import { Image } from '@chakra-ui/image';
 import { Box, Center, Flex, Text, VStack, HStack } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
+import { Tooltip } from '@chakra-ui/tooltip';
 import React, { useEffect } from 'react';
-import { getKDAStarRating } from './ChampionStats';
+import { getKDAStarRating, roundNumber } from './ChampionStats';
 import StarTag from './tags/StarTag';
 
 const getStarRatingStyle = (star) => {
-  if (star < 1 || !star) { return { color: '#ababab' }; }
-  else if (star < 2) { return { color: '#676767' }; }
-  else if (star < 3) { return { color: '#90ee90' }; }
-  else if (star < 4) { return { color: '#87cefa' }; }
-  else if (star < 5) { return { color: '#ffa500', textShadow: '0px 0px 4px #ffa500' }; }
+  if (star < 0.75 || !star) { return { color: '#ababab' }; }
+  else if (star < 1.75) { return { color: '#676767' }; }
+  else if (star < 2.75) { return { color: '#90ee90' }; }
+  else if (star < 3.75) { return { color: '#87cefa' }; }
+  else if (star < 4.75) { return { color: '#ffa500', textShadow: '0px 0px 4px #ffa500' }; }
   else { return { color: '#ff4500', textShadow: '0px 0px 4px #ff4500' }; }
 }
 
 const getBorderStyle = (star) => {
-  if (star < 2 || !star) { return {
-    border: "1px solid black"
-  }} else if (star < 3) { return {
-    border: "3px solid #90ee90"
-  }} else if (star < 4) { return {
-    border: "5px inset #87cefa"
-  }} else if (star < 5) { return {
-    border: "7px groove #ffa500"
-  }}
+  if (star < 1.75 || !star) {
+    return {
+      border: "1px solid black"
+    }
+  } else if (star < 2.75) {
+    return {
+      border: "3px solid #90ee90"
+    }
+  } else if (star < 3.75) {
+    return {
+      border: "5px inset #87cefa"
+    }
+  } else if (star < 4.75) {
+    return {
+      border: "7px groove #ffa500"
+    }
+  } else {
+    return {
+      border: "9px groove #ff4500"
+    }
+  }
 }
 
 const IconBox = ({ profile_icon_id, level, totalKDA = null, performance = null, showStarRating = true }) => {
@@ -42,10 +55,11 @@ const IconBox = ({ profile_icon_id, level, totalKDA = null, performance = null, 
         </Flex>
       }
       { showStarRating &&
-        <Flex flexDirection="row">
-          {/* {kdaStarRating(totalKDA, 5)} */}
-          <StarTag style={getStarRatingStyle(((performance * 5) + getKDAStarRating(totalKDA)) / 2)} value={((performance * 5) + getKDAStarRating(totalKDA)) / 2} />
-        </Flex>
+        <Tooltip hasArrow label={`${roundNumber(((performance * 5) + getKDAStarRating(totalKDA)) / 2)} / 5`}>
+          <Flex flexDirection="row">
+            <StarTag style={getStarRatingStyle(((performance * 5) + getKDAStarRating(totalKDA)) / 2)} value={((performance * 5) + getKDAStarRating(totalKDA)) / 2} />
+          </Flex>
+        </Tooltip>
       }
     </VStack>
   );
