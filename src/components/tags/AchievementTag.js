87c1a7b7fc2517a11fa1c642ae301ef82accaef1
Hintful <choi.kurt3@gmail.com>
2021-03-11 01:55:07 -0500

    Change achievements to use Chakra-ui tooltip for better performance

--- src/components/tags/AchievementTag.js
+++ src/components/tags/AchievementTag.js
@@ -11,46 +11,45 @@ const rarityDesc = ["Common", "Uncommon", "Rare", "Very Rare", "Extremely Rare",
 const AchievementTag = ({ achievement }) => {
   return (
     <>
-      <ReactTooltip place="top" type="dark" effect="float"/>
+      <Tooltip hasArrow label={achievement.description} bg={rarityColor[achievement.rarity]}>
         <Flex direction="row" align="center"
-        style={{ 
-          position: 'relative',
-          background: rarityColor[achievement.rarity], 
-          boxShadow: achievement.rarity >= 2 ? `0px 0px 3px ${(achievement.rarity - 1)/2}px ${rarityColor[achievement.rarity]}` : 'none',
-          fontSize: "14px", 
-          color: 'white', 
-          padding: '1px 5px', 
-          margin: '7px 5px',
-          borderRadius: '4px' }}
-          data-html={true}
-          data-tip={`<span style='font-weight: 600; color: ${rarityColor[achievement.rarity]}'>${rarityDesc[achievement.rarity]}</span><hr>${achievement.description}`}
-        >
+          style={{
+            position: 'relative',
+            background: rarityColor[achievement.rarity],
+            boxShadow: achievement.rarity >= 2 ? `0px 0px 3px ${(achievement.rarity - 1) / 2}px ${rarityColor[achievement.rarity]}` : 'none',
+            fontSize: "14px",
+            color: 'white',
+            padding: '1px 5px',
+            margin: '7px 5px',
+            borderRadius: '4px'
+          }}>
           {
             achievement.rarity === 3 ?
-            <Sparkles color="yellow" count={10} overflowPx={5} fadeoutSpeed={10} flicker={false} />
-            : achievement.rarity === 4 ? 
-            <Sparkles color="red" count={25} overflowPx={10} fadeOutSpeed={25} flicker={false} />
-            : achievement.rarity === 5 ?
-            <Sparkles color="pink" count={50} overflowPx={20} fadeOutSpeed={50} flicker={false} />
-            :
-            <>
-            </>
+              <Sparkles color="yellow" count={10} overflowPx={5} fadeoutSpeed={10} flicker={false} />
+              : achievement.rarity === 4 ?
+                <Sparkles color="red" count={25} overflowPx={10} fadeOutSpeed={25} flicker={false} />
+                : achievement.rarity === 5 ?
+                  <Sparkles color="pink" count={50} overflowPx={20} fadeOutSpeed={50} flicker={false} />
+                  :
+                  <>
+                  </>
           }
-          { 
+          {
             achievement.rarity === 0 ?
-            <BsCircleFill size="0.7em" />
-            : achievement.rarity === 1 ?
-            <BsTriangleFill size="0.8em" />
-            : achievement.rarity === 2 ?
-            <BsSquareFill size="0.8em" />
-            : achievement.rarity === 3 ?
-            <BsPentagonFill size="0.9em" />
-            : achievement.rarity === 4 ?
-            <BsStarFill />
-            : <BsFillXDiamondFill />
+              <BsCircleFill size="0.7em" />
+              : achievement.rarity === 1 ?
+                <BsTriangleFill size="0.8em" />
+                : achievement.rarity === 2 ?
+                  <BsSquareFill size="0.8em" />
+                  : achievement.rarity === 3 ?
+                    <BsPentagonFill size="0.9em" />
+                    : achievement.rarity === 4 ?
+                      <BsStarFill />
+                      : <BsFillXDiamondFill />
           }
           &nbsp;{achievement.name}
         </Flex>
+      </Tooltip>
     </>
   );
 }
